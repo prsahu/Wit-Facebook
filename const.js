8f88bb5e600e5a07b1bbca619d8584757418413d
prsahu <praffulsahu@yahoo.com>
2016-07-07 16:48:18 +0530

    Update const.js

--- const.js
+++ const.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Wit.ai parameters
-const WIT_TOKEN = process.env.WIT_TOKEN;
+const WIT_TOKEN = Q3FHWAGJSDM5WQQBTWWRIRKCDIXLO6PZ;
 if (!WIT_TOKEN) {
   throw new Error('missing WIT_TOKEN');
 }
